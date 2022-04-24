c92eef2cd414e9c0433e58251f16d6b2d9b548d9
Niels <niels.koster@portavita.nl>
2022-04-25 00:43:21 +0200

    Serve static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,8 +7,15 @@ import (
 )
 
 func routes() http.Handler {
+
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	// Serve static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
+
 	return mux
+
 }
